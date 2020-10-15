6e9d7e7c1f79da01c5f5dd92757b81bba9061b2e
Nastybits <screamie@yandex.ru>
2020-10-15 09:31:16 +0300

    Правки после ревью

--- lesson1/cmd/lesson1/main.go
+++ lesson1/cmd/lesson1/main.go
@@ -2,21 +2,21 @@
 package main
 
 import (
+	"education/lesson1/pkg/fibonacci"
 	"flag"
 	"fmt"
 	"log"
-
-	"education/lesson1/pkg/fibonacci"
 )
 
 func main() {
 	n := flag.Int("n", 1, "Индекс числа Фибоначчи")
 	flag.Parse()
 
-	num, err := fibonacci.Number(*n)
-	if err != nil {
-		log.Fatal(err)
+	if *n <= 0 || *n > 20 {
+		log.Fatal("Значение должно быть больше 0 и меньше 20")
 	}
+
+	num := fibonacci.Num(*n)
 
 	fmt.Printf("Значение числа Фибоначчи по индексу %d равно: %d", *n, num)
 }
--- lesson1/pkg/fibonacci/fibonacci_test.go
+++ lesson1/pkg/fibonacci/fibonacci_test.go
@@ -2,26 +2,24 @@ package fibonacci
 
 import "testing"
 
-func TestNumber(t *testing.T) {
+func TestNum(t *testing.T) {
 	tests := []struct {
+		name string
 		arg  int
 		want int
 	}{
-		{-1, 0},
-		{0, 0},
-		{1, 0},
-		{2, 1},
-		{3, 1},
-		{4, 2},
-		{32, 1346269},
+		{"1", -1, 0},
+		{"2", 0, 0},
+		{"3", 1, 0},
+		{"4", 2, 1},
+		{"5", 3, 1},
+		{"6", 4, 2},
+		{"7", 32, 1346269},
 	}
 
 	for _, tt := range tests {
-		t.Run("Test", func(t *testing.T) {
-			got, err := Number(tt.arg)
-			if (tt.arg <= 0 || tt.arg > 20) && err == nil {
-				t.Errorf("Number(%d) = %d, ожидалось сообщение об ошибке", tt.arg, got)
-			}
+		t.Run(tt.name, func(t *testing.T) {
+			got := Num(tt.arg)
 			if tt.arg > 0 && got != tt.want {
 				t.Errorf("Number(%d) = %d, ожидалось %d", tt.arg, got, tt.want)
 			}
