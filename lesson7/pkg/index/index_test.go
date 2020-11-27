ce19903a71c4fd9896cc1e68497951ad699ef8c9
Nastybits <screamie@yandex.ru>
2020-11-27 12:01:36 +0300

    Fix index test

--- lesson7/pkg/index/index_test.go
+++ lesson7/pkg/index/index_test.go
@@ -49,7 +49,7 @@ func TestService_Add(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.s.Add(tt.docs[0])
-			items, ok := tt.s.storage["url"]
+			items, ok := tt.s.data["url"]
 			if !ok || items[0] != 1 {
 				t.Errorf("got = %v, want %v", items, 1)
 			}
